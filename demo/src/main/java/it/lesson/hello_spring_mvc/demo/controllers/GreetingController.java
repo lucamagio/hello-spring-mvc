5e3d0a90f1f773d060439ba1059a18db64dbc42e
lucamagio <lucamagionami4@gmail.com>
2025-03-26 18:35:46 +0100

    creazione progetto con java spring e vari test fatti

---
+++ demo/src/main/java/it/lesson/hello_spring_mvc/demo/controllers/GreetingController.java
@@ -0,0 +1,25 @@
+package it.lesson.hello_spring_mvc.demo.controllers;
+
+import java.time.LocalTime;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
+public class GreetingController {
+
+@GetMapping("/greeting")
+public String greeting(@RequestParam(name = "name")String name,
+                       @RequestParam(name = "cognome") String cognome,
+                       Model model) {
+model.addAttribute("name", name);
+model.addAttribute("cognome", cognome);
+model.addAttribute("time", LocalTime.now());
+return "greeting";
+}
+
+}
